d244e2ab4ddbf3ee0ab2ec8ba8d80df17a91e7bb
fahri <akhmadfahri589@gmail.com>
2025-12-23 22:51:19 +0700

    default layout

---
+++ app/layouts/admin.vue
@@ -0,0 +1,82 @@
+<template>
+  <UDashboardGroup>
+    <!-- SIDEBAR -->
+    <UDashboardSidebar>
+      <UNavigationMenu 
+        :items="item_sidebars"
+        :collapsed="collapsed" 
+        orientation="vertical"
+        tooltip
+      />
+    </UDashboardSidebar>
+
+    <!-- MAIN -->
+    <UDashboardPanel class="flex flex-col">
+      <!-- NAVBAR -->
+      <UDashboardNavbar>
+        <template #right>
+          <UDropdownMenu :items="item_profiles">
+            <UAvatar src="https://i.pravatar.cc/40" size="sm" />
+          </UDropdownMenu>
+        </template>
+      </UDashboardNavbar>
+
+      <!-- CONTENT -->
+      <div class="flex-1 p-4">
+        <slot />
+      </div>
+    </UDashboardPanel>
+
+    <!-- LOGOUT MODAL -->
+    <UModal title="Warning" v-model:open="isLogoutModalOpen" :dismissible="false">
+        <template #body>
+            <Placeholder class="h-48" >
+                Are you sure you want to logout?
+            </Placeholder>
+        </template>
+        <template #footer>
+            <div class="flex gap-2 w-full justify-end">
+                <UButton label="Cancel" color="neutral" variant="outline" @click="close" />
+                <UButton label="Submit" color="neutral" @click="logout" />
+            </div>
+        </template>
+    </UModal>
+  </UDashboardGroup>
+</template>
+
+<script setup>
+const isLogoutModalOpen = ref(false)
+const logout = useAuth().logout
+const close = () => {
+    isLogoutModalOpen.value = false
+}
+
+const item_sidebars = ref([
+  {
+    label: 'Dashboard',
+    icon: 'i-lucide-book-open',
+    to: 'admin'
+  }
+])
+
+const item_profiles = ref([
+  [
+    {
+      label: 'Benjamin',
+      avatar: {
+        src: 'https://github.com/benjamincanac.png'
+      },
+      type: 'label'
+    }
+  ],
+  [
+    {
+      label: 'Logout',
+      icon: 'i-lucide-log-out',
+      onSelect: () => {
+        isLogoutModalOpen.value = true
+      }
+    }
+  ]
+])
+</script>
--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4">Admin Dashboard</h1>
-    <UButton color="error" @click="logout">Logout</UButton>
-  </div>
+  <NuxtLayout name="admin">
+    <div class="p-4">
+      <h1 class="text-2xl font-bold mb-4">Admin Dashboard</h1>
+    </div>
+  </NuxtLayout>
 </template>
 
 <script lang="ts" setup>
-  const { logout } = useAuth()
   definePageMeta({
     middleware: 'auth'
   })
